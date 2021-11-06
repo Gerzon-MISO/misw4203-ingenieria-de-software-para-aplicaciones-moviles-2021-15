650a5ee3b013c94795eb10c693ca73962930c556
jc-nieto <jc.nieto@uniandes.edu.co>
2021-11-06 13:34:55 -0500

    Ajustes HU02 adaptadores

--- app/src/main/java/com/example/vinyls/adapter/AlbumsAdapter.kt
+++ app/src/main/java/com/example/vinyls/adapter/AlbumsAdapter.kt
@@ -1,36 +1,39 @@
 package com.example.vinyls.adapter
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vinyls.R
 import com.example.vinyls.databinding.AlbumItemBinding
 import com.example.vinyls.models.Album
+import com.example.vinyls.ui.fragments.AlbumsFragmentDirections
 import com.squareup.picasso.Picasso
 
 
-class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>(){
+class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumsViewHolder>(){
 
+    var navController: NavController? = null
     var albums :List<Album> = emptyList()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumsViewHolder {
         val withDataBinding: AlbumItemBinding = DataBindingUtil.inflate(
             LayoutInflater.from(parent.context),
-            AlbumViewHolder.LAYOUT,
+            AlbumsViewHolder.LAYOUT,
             parent,
             false)
-        return AlbumViewHolder(withDataBinding)
+        return AlbumsViewHolder(withDataBinding)
     }
 
-    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: AlbumsViewHolder, position: Int) {
         holder.viewDataBinding.also {
-            it.album = albums[position]
+            it.albums = albums[position]
             Picasso.get()
                 .load(it.album?.cover)
                 .placeholder(R.drawable.noimg)
@@ -39,7 +42,8 @@ class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>(){
         }
 
         holder.viewDataBinding.root.setOnClickListener {
-            println(albums[position].albumId)
+            val action = AlbumsFragmentDirections.actionAlbumsFragmentToAlbumDetailFragment(albums[position].albumId)
+            holder.viewDataBinding.root.findNavController().navigate(action)
         }
     }
 
@@ -47,7 +51,7 @@ class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>(){
         return albums.size
     }
 
-    class AlbumViewHolder(val viewDataBinding: AlbumItemBinding) :
+    class AlbumsViewHolder(val viewDataBinding: AlbumItemBinding) :
 
         RecyclerView.ViewHolder(viewDataBinding.root) {
         companion object {
