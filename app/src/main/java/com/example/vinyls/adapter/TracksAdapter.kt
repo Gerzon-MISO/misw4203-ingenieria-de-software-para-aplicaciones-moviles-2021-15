5c9e3923cc1cdd8ec2f5b579959d4fa73d57c76e
Gerzon <gerzon.bautista@dafiti.com.co>
2021-11-07 07:51:59 -0500

    Implementación listado de tracks en detalle album

---
+++ app/src/main/java/com/example/vinyls/adapter/TracksAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.vinyls.adapter
+
+class TracksAdapter {
+}
---
+++ app/src/main/java/com/example/vinyls/models/Track.kt
@@ -0,0 +1,7 @@
+package com.example.vinyls.models
+
+class Track (
+    val trackId: Int,
+    val name: String,
+    val duration: String
+)
