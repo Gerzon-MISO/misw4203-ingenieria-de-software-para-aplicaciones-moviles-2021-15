f42887533f9d25e92cf5cd987d9a4464aa5a55ee
jc-nieto <jc.nieto@uniandes.edu.co>
2021-11-16 02:03:07 -0500

    Creacion y ajustes viewmodels

--- app/src/main/java/com/example/vinyls/viewmodels/MusiciansViewModel.kt
+++ app/src/main/java/com/example/vinyls/viewmodels/MusiciansViewModel.kt
@@ -2,17 +2,17 @@ package com.example.vinyls.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.*
-import com.example.vinyls.models.Band
+import com.example.vinyls.models.Musician
 import com.example.vinyls.repositories.ArtistRepository
 
-class BandsViewModel(application: Application) : AndroidViewModel(application) {
+class MusiciansViewModel(application: Application) : AndroidViewModel(application) {
 
     private val artistRepository = ArtistRepository(application)
 
-    private val _bands = MutableLiveData<List<Band>>()
+    private val _musicians = MutableLiveData<List<Musician>>()
 
-    val bands:LiveData<List<Band>>
-        get() = _bands
+    val bands:LiveData<List<Musician>>
+        get() = _musicians
 
     private  var _eventNetworkError = MutableLiveData<Boolean>(false)
 
@@ -29,8 +29,8 @@ class BandsViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun refreshDataFromNetwork() {
-        artistRepository.refreshBandsData({
-            _bands.postValue(it)
+        artistRepository.refreshMusiciansData({
+            _musicians.postValue(it)
             _eventNetworkError.value = false
             _isNetworkErrorShown.value = false
         },{
@@ -44,9 +44,9 @@ class BandsViewModel(application: Application) : AndroidViewModel(application) {
 
     class Factory(val app: Application) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(BandsViewModel::class.java)) {
+            if (modelClass.isAssignableFrom(MusiciansViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return BandsViewModel(app) as T
+                return MusiciansViewModel(app) as T
             }
             throw IllegalArgumentException("Unable to construct viewmodel")
         }
