55b5aaf6c3bb800d78f1b5c61588b5f9b261f2b3
Gerzon <gerzon.bautista@dafiti.com.co>
2021-11-15 20:19:18 -0500

    Creación del layout para la HU de asociar canción a un album

--- app/src/main/java/com/example/vinyls/ui/fragments/AlbumDetailFragment.kt
+++ app/src/main/java/com/example/vinyls/ui/fragments/AlbumDetailFragment.kt
@@ -1,38 +1,25 @@
 package com.example.vinyls.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import android.widget.Toolbar
-import androidx.activity.addCallback
+import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.core.app.ApplicationProvider
-import com.example.vinyls.R
 import com.example.vinyls.adapter.AlbumAdapter
 import com.example.vinyls.adapter.AlbumArtistsAdapter
 import com.example.vinyls.adapter.AlbumTracksAdapter
 import com.example.vinyls.databinding.FragmentAlbumDetailBinding
 import com.example.vinyls.models.Album
 import com.example.vinyls.viewmodels.AlbumViewModel
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
-
-
-
-
-
-
+import com.example.vinyls.R
 
 
 class AlbumDetailFragment : Fragment() {
@@ -54,6 +41,13 @@ class AlbumDetailFragment : Fragment() {
         _binding = FragmentAlbumDetailBinding.inflate(inflater, container, false)
         val view = binding.root
         viewModelAdapter = AlbumAdapter()
+
+        val createTrackButton : AppCompatButton = view.findViewById(R.id.agregar_cancion_but)
+        createTrackButton.setOnClickListener {
+            val fragmentCreateTrack = CreateTrackFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.nav_host_fragment, fragmentCreateTrack)?.commit()
+        }
 
         return view
     }
