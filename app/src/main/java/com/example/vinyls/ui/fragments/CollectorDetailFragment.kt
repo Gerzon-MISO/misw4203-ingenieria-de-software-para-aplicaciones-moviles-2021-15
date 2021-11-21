91afa18c78956859fb15f7f750c742f5fa1b9e28
Gerzon <gerzon.bautista@dafiti.com.co>
2021-11-21 12:59:42 -0500

    Micro-optimizacion android_resources_validation

--- app/src/main/java/com/example/vinyls/ui/fragments/CollectorDetailFragment.kt
+++ app/src/main/java/com/example/vinyls/ui/fragments/CollectorDetailFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentCollectorDetail.newInstance] factory method to
+ * Use the [CollectorDetailFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentCollectorDetail : Fragment() {
+class CollectorDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -45,12 +45,12 @@ class FragmentCollectorDetail : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentCollectorDetail.
+         * @return A new instance of fragment CollectorDetailFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentCollectorDetail().apply {
+            CollectorDetailFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
